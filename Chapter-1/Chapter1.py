a1550d9556ce5f0111df11b3f93f844aeb8bbcd8
nuhfurkan <65976979+nuhfurkan@users.noreply.github.com>
2021-10-23 13:53:09 +0300

    Update Chapter1.py

--- Chapter-1/Chapter1.py
+++ Chapter-1/Chapter1.py
@@ -1,13 +1,15 @@
+import matplotlib.pyplot as plt
+
 #Finding a name
 def example1():
-    file = open("name")
+    file = open("Chapter 1/name")
     for line in file:
         if line.startswith("John"):
             print(line)
 
 # Finding an email
 def example2():
-    file = open("emails")
+    file = open("Chapter 1/emails")
     for line in file:
         name, email = line.split(",")
         if (name == "John Wick"):
@@ -15,14 +17,20 @@ def example2():
 
 # Average of a collection of observations
 def example3():
+    temps = []
+    days = []
     sum = 0
     count = 0
-    file = open("observations")
+    file = open("Chapter 1/observations")
     for line in file:
         sum += float(line)
         count += 1
+        temps.append(float(line))
+        days.append(int(count))
 
     print("Sum is " + str(sum))
     print("Average is " + str(round(sum/count, 2)))
+    plt.plot(days, temps, "o")
+    plt.show()
 
 example3()
